f451da1afdafe19231aaf2ce4af43b6fa13d5e17
Avi Agarwal <avimailg2@gmail.com>
2021-08-23 20:16:48 +0530

    Final build2
    Co-Authored-By: scoowin <61704171+scoowin@users.noreply.github.com>

---
+++ app/app/src/main/java/com/example/tekhawk/myapplication/OurBro.java
@@ -0,0 +1,31 @@
+package com.example.tekhawk.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by TekHa on 25-12-2017.
+ */
+
+public class OurBro extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.our_bro, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Diabytes");
+    }
+}
